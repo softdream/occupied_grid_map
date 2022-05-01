f5c5cc39958dc67473639fd260d779c980fce6da
daofeng <9779716+du-daofeng@user.noreply.gitee.com>
2022-05-02 02:37:56 +0800

    occupied grid map

---
+++ occupied_grid_map.h
@@ -0,0 +1,189 @@
+#ifndef __OCCUPIED_GRID_MAP
+#define __OCCUPIED_GRID_MAP
+
+#include "grid_map_base.h"
+
+#include "dataContainer.h"
+
+namespace grid
+{
+
+template<typename T>
+using GridMapBaseType = GridMapBase<T, GridCell, GridCellOperations>;
+
+template<typename T, typename = typename std::enable_if<is_double_or_float<T>::value>::type>
+class OccupiedGridMap : public GridMapBaseType<T>
+{
+public:
+	using ConstSharedPtr = const std::shared_ptr<OccupiedGridMap<T>>;
+	using SharedPtr = std::shared_ptr<OccupiedGridMap<T>>;
+	using DataType = typename T;
+	
+	OccupiedGridMap()
+	{
+
+	}
+
+	~OccupiedGridMap()
+	{
+	
+	}
+
+	OccupiedGridMap( const int map_size_x, const int map_size_y, const DataType cell_length ) : GridMapBaseType<T>( map_size_x, map_size_y, cell_length )
+	{
+		
+	}
+
+	const Eigen::Matrix<DataType, 2, 1> observedPointLaser2World( const Eigen::Matrix<DataType, 2, 1> &point_in_laser, const Eigen::Matrix<DataType, 3, 1> &robot_pose_in_world ) const
+	{
+		Eigen::Matrix<DataType, 2, 2> rotate;
+		rotate << ::cos( robot_pose_in_world[2] ), -::sin( robot_pose_in_world[2] ),
+			  ::sin( robot_pose_in_world[2] ),  ::cos( robot_pose_in_world[2] );
+		return rotate * point_in_laser + robot_pose_in_world.head<2>();
+	}
+
+	const Eigen::Matrix<DataType, 2, 1> observedPointWorld2Laser( const Eigen::Matrix<DataType, 2, 1> &point_in_world, const Eigen::Matrix<DataType, 3, 1> &robot_pose_in_world ) const
+	{
+		Eigen::Matrix<DataType, 2, 2> rotate;
+                rotate << ::cos( robot_pose_in_world[2] ), -::sin( robot_pose_in_world[2] ),
+                          ::sin( robot_pose_in_world[2] ),  ::cos( robot_pose_in_world[2] );
+		return rotate.inverse() * point_in_world - robot_pose_in_world.head<2>();
+	}
+
+	template<typename =  std::enable_if<std::is_same<DataType, sensor::ScanContainer::value_type>::value>::type>
+	void updateMapByScan( const sensor::ScanContainer &scan, const Eigen::Matrix<DataType, 3, 1> &robot_pose_in_world )
+	{
+		// 1. Transform robot Pose In world Coordinate to Map Coordinat
+        	Eigen::Matrix<DataType, 3, 1> robot_pose_in_map = this->robotPoseWorld2Map( robot_pose_in_world );
+		// std::cout<<"Robot Pose In Map Coordinate: "<<std::endl;
+        	// std::cout<<robotPoseInMap<<std::endl;
+	
+		// 2. Get the start point of the laser data in Map Coordinate
+        	Eigen::Vector2i begin_point_in_map_i( robot_pose_in_map.head<2>().cast<int>() );
+		
+		size_t numberOfBeams = points.getSize();
+        	//std::cout<<"Number Of Beams: "<<numberOfBeams<<std::endl;
+		for( size_t i = 0; i < numberOfBeams; i ++ ){
+			// 3. Get the End point of Every Laser Beam in Laser Coordinate
+                	Eigen::Matrix<DataType, 2, 1> end_point_in_laser = points.getIndexData( i ) ;
+		
+			// 4. Transform the End Point from Laser Coordinate to World Coordinate
+                	Eigen::Matrix<DataType, 2, 1> end_point_in_world( this->observedPointLaser2World( end_point_in_laser, robot_pose_in_world ) );
+
+			// 5. Transform the End Point from World Coordinate to Map Coordinate
+                	Eigen::Matrix<DataType, 2, 1> end_point_in_map( this->observedPointWorld2Map( end_point_in_world ) );
+
+			// 6. Convert float to interger
+			Eigen::Vector2i end_point_in_map_i( static_cast<int>( ::round( end_point_in_map[0] ) ), static_cast<int>( ::round( end_point_in_map[1] ) ) );
+
+			// 7. execuate Inverse Model algorithm
+                	if( end_point_in_map_i != begin_point_in_map_i ){
+                        	this->inverseModel( begin_point_in_map_i, end_point_in_map_i );
+                	}
+		}
+	}
+
+        void clearGridMap()
+	{
+		return this->mapArrayClear();
+	}
+	
+	void resetGridMap()
+	{
+		for( size_t i = 0; i < this->map_array_.size(); i ++ ){
+			this->map_array_[i].log_odds_value_ = 0;
+		}
+	}
+
+private:	
+	void inverseModel( const int x0, const int y0, const int x1, const int y1 )
+        {
+		// 1. set the end point occupied first
+        	bresenhamCellOccupied( x1, y1 );
+		
+		//std::cout<<"Occupied Cell Pose In Map: ( "<< x1 <<", "<< y1<<" )"<<std::endl;
+		// 2. execute the bresenham algorithm, find the points of free, and set them free
+        	bresenHam( x0, y0, x1, y1 );
+        }
+
+        void inverseModel( const Eigen::Vector2i &p0, const Eigen::Vector2i &p1 )
+        {
+         	return inverseModel( p0[0], p0[1], p1[0], p1[1] );
+        }
+	
+
+	void bresenHam( const int x0, const int y0, const int x1, const int y1 )
+	{
+		int dx = ::abs( x1 - x0 );
+        	int dy = ::abs( y1 - y0 );
+
+        	bool interChange = false;
+
+        	int e = -dx;// error
+
+        	int signX = x1 > x0 ? 1 : ( ( x1 < x0 ) ? -1 : 0 );
+        	int signY = y1 > y0 ? 1 : ( ( y1 < y0 ) ? -1 : 0 );
+
+        	if (dy > dx) {
+                	int temp = dx;
+                	dx = dy;
+                	dy = temp;
+                	interChange = true;
+        	}
+	
+		int x = x0, y = y0;
+        	for (int i = 1; i <= dx; i++) { // not include the end point
+                	//operate( x, y, img1 );
+                	// add operations for the point 
+                	bresenhamCellFree( x, y );
+
+                	if (!interChange)
+                        	x += signX;
+                	else
+                        	y += signY;
+
+                	e += 2 * dy;
+
+                	if (e >= 0) {
+                        	if (!interChange)
+                                	y += signY;
+                        	else
+                                	x += signX;
+
+                        	e -= 2 * dx;
+                	}
+        	}
+	}
+        
+	void bresenHam( Eigen::Vector2i &p0, Eigen::Vector2i &p1)
+	{
+		return bresenHam( p0[0], p0[1], p1[0], p1[1] );
+	}
+
+        void bresenhamCellFree( const int index )	
+	{
+		//CellType &cell = this->getCell( index );
+		this->setCellFree( index );
+	}
+
+	void bresenhamCellFree( const int x, const int y )
+        {
+		this->setCellFree( x, y );
+        }
+
+        void bresenhamCellOccupied( const int index )
+	{
+		this->setCellOccupied( index );	
+	}
+
+        void bresenhamCellOccupied( const int x, const int y )
+	{
+		this->setCellOccupied( x, y );
+	}
+
+};
+
+}
+
+
+#endif
